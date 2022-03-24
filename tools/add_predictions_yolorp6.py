a3d971b9b45d8250191aa794f739c3ed53334800
Patrick Schmidt <patschmidt1234@gmail.com>
2022-03-24 14:38:17 +0100

    fixed yolor prediction format

--- tools/add_predictions_yolorp6.py
+++ tools/add_predictions_yolorp6.py
@@ -25,6 +25,10 @@ with fo.ProgressBar() as pb:
                 prediction = prediction.split(' ')
                 #Don't add every prediction (ran the detection with conf 0.0, so absolutely no NMS or anything)
                 if float(prediction[5]) > 0.005:
+
+                    #xywh in file, where xy are CENTER coordinates, not top left, top right
+                    prediction[1] = float(prediction[1]) - 0.5*float(prediction[3])
+                    prediction[2] = float(prediction[2]) - 0.5*float(prediction[4])
                     detections.append(
                         fo.Detection(
                             label=classes[int(prediction[0])],
