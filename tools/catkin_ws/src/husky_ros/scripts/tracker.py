637da4c3d1ca4ed18b8455f64be543e0a3634271
Patrick Schmidt <patschmidt1234@gmail.com>
2022-06-29 21:31:32 +0200

    made tracker python2 compatible

--- tools/catkin_ws/src/husky_ros/scripts/tracker.py
+++ tools/catkin_ws/src/husky_ros/scripts/tracker.py
@@ -1,12 +1,17 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
+import sys
 import rospy
 import numpy as np
-import scipy as sp
+#import scipy as sp
 import copy
 import cv2
-import torch
+#import torch
 import time
+import tf
 
+#Little trick for mixed python2-python3 environments, cv_bridge is compiled with python3 too.
+#This one uses python2 and cv_bridge, thus we need to make sure that the python2 version of cv_bridge is loaded
+sys.path.insert(0, '/opt/ros/melodic/lib/python2.7/dist-packages')
 
 from sensor_msgs.msg import Image
 from vision_msgs.msg import Detection2DArray
@@ -14,10 +19,12 @@ from cv_bridge import CvBridge
 from std_msgs.msg import String, Float32
 from husky_ros.msg import Detection2DArrayWithImage
 
+
+
 #from yolor/utils/general.py
 def xywh2xyxy(x):
     # Convert nx4 boxes from [x, y, w, h] to [x1, y1, x2, y2] where xy1=top-left, xy2=bottom-right
-    y = x.clone() if isinstance(x, torch.Tensor) else np.copy(x)
+    y =  np.copy(x)
     y[:, 0] = x[:, 0] - x[:, 2] / 2  # top left x
     y[:, 1] = x[:, 1] - x[:, 3] / 2  # top left y
     y[:, 2] = x[:, 0] + x[:, 2] / 2  # bottom right x
@@ -74,22 +81,22 @@ class KalmanNd:
             self.P_hist.append(copy.deepcopy(self.P))
             self.P = np.dot(np.dot(self.F, self.P), self.F.T)
             self.updated.append(False)
-    def get_ellipsis(self, conf=0.95):
-        """Returns the center point, minor and major axis of the ellipsis"""
+    # def get_ellipsis(self, conf=0.95):
+    #     """Returns the center point, minor and major axis of the ellipsis"""
         
-        #Get the observables (columns/rows)
-        axs = [x[0] for x in np.argwhere(self.H)]
-        #Construct the custom covariance matrix
-        P_sub = self.P[np.ix_(axs, axs)]
-        #Get the eigenvectors
-        ev, evec = sp.linalg.eig(P_sub)
-        conf_length = sp.stats.chi2.ppf(conf, P_sub.shape[0])
-        #Note that this is just half the length (so you can do center +- length)
-        axis_length = np.sqrt(ev*conf_length)
-        ord = np.argsort(axis_length)
+    #     #Get the observables (columns/rows)
+    #     axs = [x[0] for x in np.argwhere(self.H)]
+    #     #Construct the custom covariance matrix
+    #     P_sub = self.P[np.ix_(axs, axs)]
+    #     #Get the eigenvectors
+    #     ev, evec = sp.linalg.eig(P_sub)
+    #     conf_length = sp.stats.chi2.ppf(conf, P_sub.shape[0])
+    #     #Note that this is just half the length (so you can do center +- length)
+    #     axis_length = np.sqrt(ev*conf_length)
+    #     ord = np.argsort(axis_length)
 
-        #center, minor, major (actually, need to adjust this to be generalized to n-d, not only 2d)
-        return self.x[axs], axis_length[ord[0]]*(evec[ord[0]]/np.linalg.norm(evec[ord[0]])), axis_length[ord[1]]*(evec[ord[1]]/np.linalg.norm(evec[ord[1]]))
+    #     #center, minor, major (actually, need to adjust this to be generalized to n-d, not only 2d)
+    #     return self.x[axs], axis_length[ord[0]]*(evec[ord[0]]/np.linalg.norm(evec[ord[0]])), axis_length[ord[1]]*(evec[ord[1]]/np.linalg.norm(evec[ord[1]]))
 
     def reset(self):
         self.__init__(self.x_initial, self.P_initial, self.H, self.R, self.F)
@@ -129,11 +136,20 @@ class TrackingNode:
         self.state_pub = rospy.Publisher("tracker/tracker_state", String, queue_size=10)
         self.sift_time_pub = rospy.Publisher("tracker/sift_time/detect", Float32, queue_size=10)
         self.sift_time_match_pub = rospy.Publisher("tracker/sift_time/match", Float32, queue_size=10)
+        self.detection_count_pub = rospy.Publisher("/tracker/detection_count", Float32, queue_size=10)
         self._dilateelement = cv2.getStructuringElement(cv2.MORPH_RECT, (201,201))
+        self.tf_listener = tf.TransformListener()
+        self.detection_database = []
         pass
     
     def handle_image(self, msg):
-        
+        trans, rot = None, None
+        try:
+            trans, rot = self.tf_listener.lookupTransform('odom', 'camera_link', rospy.Time(0))
+        except (tf.LookupException, tf.ConnectivityException, tf.ExtrapolationException):
+           pass 
+        rospy.loginfo("Current location of camera is: x/y/z {}".format(trans)) 
+    
         #Get length of detections
         if len(msg.detections) > 0 and abs(self.detection_count - len(msg.detections)) == 0 :
             #NMS + confidence thresholded detections available
@@ -153,10 +169,14 @@ class TrackingNode:
         elif len(msg.detections) == 0:
             #Transition to none_available state
             self.state = 'none_detected' 
+            self.detection_count = 0
         
         if self.state in ["detected_new", "detected_loss", "none_detected"]:
             #Calculate features in ROIs (detected areas)
             
+            #Empty array, holds bounding box candidates from recieved detections
+            detections = []
+
             img = self._bridge.imgmsg_to_cv2(msg.source_img.img)
             if self.state != "none_detected":
                 box_mask = np.zeros((msg.source_img.img.height, msg.source_img.img.width), dtype=np.uint8)
@@ -165,6 +185,7 @@ class TrackingNode:
             for detection in msg.detections:
                 bbox = np.asarray([detection.bbox.center.x, detection.bbox.center.y,
                              detection.bbox.size_x, detection.bbox.size_y]).reshape(1,4)
+                detections.append(bbox)
                 tl_x, tl_y, br_x, br_y = np.squeeze(xywh2xyxy(bbox), axis=0).astype(int)
                 box_mask[tl_y:(br_y+1), tl_x:(br_x+1)] = 1
                 box_mask = cv2.dilate(box_mask, self._dilateelement)
@@ -172,6 +193,11 @@ class TrackingNode:
             kp, des = self.feature_tracker.detectAndCompute(cv2.cvtColor(img, cv2.COLOR_BGR2GRAY), box_mask)
             t1 = time.time()
             self.sift_time_pub.publish(Float32(t1-t0))
+
+            #Compare candidate boxes with boxes from previous pictures
+            if len(self.detection_database) > 0:
+                det_curr = np.asarray(detections)
+                
 
             if self.state == "detected_new":
                 self.kp, self.des = kp, des
@@ -198,6 +224,7 @@ class TrackingNode:
             self.vis_match_pub.publish(self._bridge.cv2_to_imgmsg(img_match))
 
         self.state_pub.publish(self.state)
+        self.detection_count_pub.publish(Float32(self.detection_count))
 
         
 
